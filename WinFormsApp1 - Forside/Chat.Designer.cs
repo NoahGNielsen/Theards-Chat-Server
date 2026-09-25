aadbb04da5f0f4bdc8c5d2422b613aba6f4b655d
Elias Mølgaard Appel <elia1394@mail.hansenberg.dk>
2026-09-25 09:46:27 +0200

    Lavet chat formsen

--- WinFormsApp1 - Forside/Chat.Designer.cs
+++ WinFormsApp1 - Forside/Chat.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonSend = new Button();
+            label1 = new Label();
+            listBoxChat = new ListBox();
+            textBoxBesked = new TextBox();
+            SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            buttonSend.Location = new Point(509, 398);
+            buttonSend.Name = "buttonSend";
+            buttonSend.Size = new Size(269, 29);
+            buttonSend.TabIndex = 0;
+            buttonSend.Text = "Send besked";
+            buttonSend.UseVisualStyleBackColor = true;
+            buttonSend.Click += buttonSend_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(386, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // listBoxChat
+            // 
+            listBoxChat.FormattingEnabled = true;
+            listBoxChat.Location = new Point(24, 42);
+            listBoxChat.Name = "listBoxChat";
+            listBoxChat.Size = new Size(754, 304);
+            listBoxChat.TabIndex = 2;
+            listBoxChat.SelectedIndexChanged += listBoxChat_SelectedIndexChanged;
+            // 
+            // textBoxBesked
+            // 
+            textBoxBesked.Location = new Point(24, 400);
+            textBoxBesked.Name = "textBoxBesked";
+            textBoxBesked.Size = new Size(445, 27);
+            textBoxBesked.TabIndex = 3;
+            textBoxBesked.TextChanged += textBoxBesked_TextChanged;
+            // 
+            // Chat
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBoxBesked);
+            Controls.Add(listBoxChat);
+            Controls.Add(label1);
+            Controls.Add(buttonSend);
+            Name = "Chat";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonSend;
+        private Label label1;
+        private ListBox listBoxChat;
+        private TextBox textBoxBesked;
     }
 }
